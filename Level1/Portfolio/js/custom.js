5bbec5f727df69609b2880c569a4dc61eca14c6d
itzSerag <serag.eldien.mahmoud@gmail.com>
2023-09-09 00:18:10 +0300

    portfolio_done

---
+++ Level1/Portfolio/js/custom.js
@@ -0,0 +1,31 @@
+(function ($) {
+  "use strict";
+
+  // COLOR MODE
+  $('.color-mode').click(function() {
+      $('.color-mode-icon').toggleClass('active');
+      $('body').toggleClass('dark-mode');
+  });
+
+  // HEADER // for when i scroll up the page or scroll down
+  $(".navbar").headroom();
+
+  // PROJECT CAROUSEL
+  $('.owl-carousel').owlCarousel({
+      items: 1,
+      loop: true,
+      margin: 10,
+      nav: true
+  });
+
+  // SMOOTHSCROLL
+  $('.nav-link, .custom-btn-link').on('click', function(event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+          scrollTop: $($anchor.attr('href')).offset().top 
+      }, 1000);
+      event.preventDefault();
+  });
+
+
+})(jQuery);
